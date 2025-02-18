a62b65a517910febab1ec7c14bcc4ce59e9edfac
afrina <afrinaghafar99@gmail.com>
2025-02-18 10:26:59 +0800

    First Push

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Sentiment Analysis On Amazon Fine Food Reviews',
+    author='afrina',
+    license='',
+)
